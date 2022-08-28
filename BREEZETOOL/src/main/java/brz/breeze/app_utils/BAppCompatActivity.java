3640e63f838530da76fa84f17f26da47db2afe82
BREEZE <2199825963@qq.com>
2022-08-28 21:23:08 +0800

    新增BApplication

--- BREEZETOOL/src/main/java/brz/breeze/app_utils/BAppCompatActivity.java
+++ BREEZETOOL/src/main/java/brz/breeze/app_utils/BAppCompatActivity.java
@@ -10,7 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Map;
 
-public abstract class BAcitivityCompat extends AppCompatActivity {
+public abstract class BAppCompatActivity extends AppCompatActivity {
 
     public SharedPreferences preference;
 
